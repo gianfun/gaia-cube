7e07baf092800491cd1c44a851b7d08d03d1f2c1
gianfranco <gianfranco@fanatee.com>
2016-05-01 00:33:00 -0300

    Begin Controller and gesture recognition
    A few gestures are now recognized. The GestureDetector must be added
    to an IHandModel (such as the Capsule Hand). It can detect:
     - pinches (using thumb and index finger),
     - number of fingers extended
     - if all fingers are extended
     - if fingers are all close to each other (like a paw)
    This should help us further on. Other gestures will also be added.
    
    A few materials were added to make development easier to see.

---
+++ GaiaCube/Assets/Scripts/GestureDetector.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GestureDetector : MonoBehaviour {
+	[SerializeField]
+	public Leap.Unity.IHandModel _handModel;
+
+	[SerializeField]
+	protected GameObject _cube;
+
+	[SerializeField]
+	protected Camera _camera;
+
+	[SerializeField]
+	public GameObject _plane;
+
+	public Leap.Hand hand;
+	public bool isPinching = false;
+	public bool isOpenHand = false;
+	public bool isPaw = false;
+
+	public Vector3 pinchPosition;
+	public int extendedFingers;
+	public float fingerDistance;
+
+	private Collider plane;
+	// Use this for initialization
+	void Start () {
+		hand = _handModel.GetLeapHand ();
+		plane = _plane.GetComponent<Collider>();
+
+		//plane = (Plane)_plane;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hand = _handModel.GetLeapHand ();
+		if (hand != null || !_handModel.IsTracked) {
+			isPinching = false;
+			isOpenHand = false;
+			isPaw = false;
+			extendedFingers = 0;
+			fingerDistance = 0f;
+
+			//Vector3 pos = hand.PalmPosition.ToVector3(); 
+			pinchPosition = Vector3.zero;
+			Vector3 handNormal = hand.PalmNormal.ToVector3();
+			var fingers = hand.Fingers;
+
+			if (hand.PinchDistance < 30f) {
+				for (int i = 0; i < fingers.Count; i++) {
+					var finger = fingers [i];
+					if (finger.Type == Leap.Finger.FingerType.TYPE_INDEX ||
+					    finger.Type == Leap.Finger.FingerType.TYPE_THUMB) {
+						Leap.Vector v = finger.Bone (Leap.Bone.BoneType.TYPE_DISTAL).NextJoint;
+						pinchPosition += new Vector3(v.x, v.y, v.z);
+					}
+				}
+				pinchPosition /= 2.0f;
+				isPinching = true;
+			}
+
+			fingerDistance = 0f;
+			for (int i = 0; i < fingers.Count; i++) {
+				var finger = fingers [i];
+				Vector3 fDir = finger.Bone (Leap.Bone.BoneType.TYPE_DISTAL).Direction.ToVector3();
+				if (Vector3.Dot(fDir, handNormal) < 0.40f && finger.IsExtended) {
+					extendedFingers += 1;
+				}
+
+				if (finger.Type == Leap.Finger.FingerType.TYPE_MIDDLE ||
+					finger.Type == Leap.Finger.FingerType.TYPE_RING ||
+					finger.Type == Leap.Finger.FingerType.TYPE_PINKY) {
+					fingerDistance += Vector3.Distance (finger.TipPosition.ToVector3(), fingers [i - 1].TipPosition.ToVector3());
+				}
+			}
+			fingerDistance /= 3;
+
+			print ("Finger dist: " + fingerDistance);
+			print ("We have " + extendedFingers + " extended fingers!");
+			if (extendedFingers == 5) {
+				isOpenHand = true;
+			}
+			if (isOpenHand && fingerDistance < 0.021f) {
+				isPaw = true;
+				print ("Paw!");
+			}
+		} else {
+			isPinching = false;
+			isOpenHand = false;
+			isPaw = false;
+			extendedFingers = 0;
+			fingerDistance = 0f;
+		}
+	}
+}
+
---
+++ GaiaCube/Assets/Scripts/GestureManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class UnityVectorExtension {
+
+	/**
+    * Converts a Leap Vector object to a UnityEngine Vector3 object.
+    *
+    * Does not convert to the Unity left-handed coordinate system or scale
+    * the coordinates from millimeters to meters.
+    */
+	public static Vector3 ToVector3(this Leap.Vector vector) {
+		return new Vector3(vector.x, vector.y, vector.z);
+	}
+}
+
+public class GestureManager : MonoBehaviour {
+	[SerializeField]
+	public GestureDetector gd1;
+	[SerializeField]
+	public GestureDetector gd2;
+	[SerializeField]
+	public Camera _camera;
+
+	[SerializeField]
+	public GameObject highlighter;
+
+
+	[SerializeField]
+	public Collider plane;
+
+
+	private GestureDetector left;
+	private GestureDetector right;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 topLeftSelection = Vector3.zero;
+		Vector3 bottomRightSelection = Vector3.zero;
+		if (gd1.hand.IsLeft) {
+			left = gd1;
+			right = gd2;
+		} else {
+			left = gd2;
+			right = gd1;
+		}
+
+		if (left.isPinching && right.isPinching) {
+			topLeftSelection = getPlanePoint (left.pinchPosition);
+			bottomRightSelection = getPlanePoint (right.pinchPosition);
+			Vector3 tl = topLeftSelection;
+			Vector3 br = bottomRightSelection;
+
+			highlighter.transform.position = new Vector3 ((tl.x + br.x) / 2, (tl.y + br.y) / 2, (tl.z + br.z) / 2);
+			highlighter.transform.localScale = new Vector3 ((tl.x - br.x) , 1f, (tl.z - br.z) );
+			highlighter.transform.rotation = Quaternion.identity;
+		}
+	}
+
+	public Vector3 getPlanePoint(Vector3 pinchPoint){
+		Vector3 camPos = _camera.transform.position; 
+		Ray ray = new Ray (camPos, pinchPoint - camPos);
+		RaycastHit rayHit;
+		if (plane.Raycast (ray, out rayHit, 100f)) {
+//			print ("Hit: " + rayHit.point);
+			Vector3 hitPoint = rayHit.point;
+			return new Vector3 (hitPoint.x, hitPoint.y, hitPoint.z);
+		} else {
+			return Vector3.zero;
+		}
+	}
+}
