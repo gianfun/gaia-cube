4ef8897447998e9ae908f90e446e9fa039637443
Vinícius Pinto Menézio <v.menezio@gmail.com>
2016-05-30 03:59:47 -0300

    Adding template for fire mechanics.

---
+++ GaiaCube/Assets/Scripts/FireController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireController : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown ("2")) {
+			GameObject world = GameObject.FindGameObjectWithTag ("World");
+			Transform hoveredBlock = world.GetComponent<WorldController> ().GetHovered ();
+			DryOutPoolSlice (world, hoveredBlock);
+		}
+	}
+
+	private void DryOutPoolSlice (GameObject world, Transform hoveredBlock) {
+	
+	}
+}
